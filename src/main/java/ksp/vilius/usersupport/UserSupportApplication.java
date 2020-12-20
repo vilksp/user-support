726003525d01422e35ad6d346a20fe27fa0fba74
vilksp <v.kasperavicius1@gmail.com>
2020-12-20 19:44:36 +0200

    initial commit

---
+++ src/main/java/ksp/vilius/usersupport/UserSupportApplication.java
@@ -0,0 +1,13 @@
+package ksp.vilius.usersupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ksp/vilius/usersupport/UserSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package ksp.vilius.usersupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
