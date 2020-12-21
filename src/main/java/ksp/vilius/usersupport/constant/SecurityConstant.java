da63a460f2973f8c6559fd67f349b22495e0d38e
vilksp <v.kasperavicius1@gmail.com>
2020-12-21 19:21:25 +0200

    Jwt token provider(beta version) added

--- src/main/java/ksp/vilius/usersupport/constant/SecurityConstant.java
+++ src/main/java/ksp/vilius/usersupport/constant/SecurityConstant.java
@@ -5,7 +5,7 @@ public class SecurityConstant {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String JWT_TOKEN_HEADER = "Jwt-Token";
     public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
-    public static final String GET_ARRAYS_LLC = "Get Arrays,LLC";
+    public static final String GET_VILKSP_LLC = " vilksp,LLC";
     public static final String GET_ADMINISTRATION = "User Management Portal";
     public static final String AUTHORITIES = "authorities";
     public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
---
+++ src/main/java/ksp/vilius/usersupport/utility/JwtTokenProvider.java
@@ -0,0 +1,107 @@
+package ksp.vilius.usersupport.utility;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import ksp.vilius.usersupport.models.UserPrincipal;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static java.util.Arrays.stream;
+import static ksp.vilius.usersupport.constant.SecurityConstant.*;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(UserPrincipal userPrincipal) {
+        String[] claims = getClaims(userPrincipal);
+        return JWT.create()
+                .withIssuer(GET_VILKSP_LLC)
+                .withAudience(GET_ADMINISTRATION)
+                .withIssuedAt(new Date())
+                .withSubject(userPrincipal.getUsername())
+                .withArrayClaim(AUTHORITIES, claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(HMAC512(secret));
+    }
+
+    public List<GrantedAuthority> getAuthorities(String token) {
+        String[] claims = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new
+                UsernamePasswordAuthenticationToken(username, null, authorities);
+
+        usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        return usernamePasswordAuthenticationToken;
+
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+    }
+
+    public String getSubject(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+
+        return verifier.verify(token).getSubject();
+    }
+
+    private JWTVerifier getJWTVerifier() {
+
+        JWTVerifier verifier;
+        try {
+            Algorithm algorithm = HMAC512(secret);
+            verifier = JWT.require(algorithm)
+                    .withIssuer(GET_VILKSP_LLC)
+                    .build();
+        } catch (JWTVerificationException e) {
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return verifier;
+    }
+
+    public boolean isTokenValid(String username, String token) {
+        JWTVerifier verifier = getJWTVerifier();
+
+        return StringUtils.isNotEmpty(username) && !isTokenExpired(verifier, token);
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier, String token) {
+        Date expiration = verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
+    }
+
+
+    private String[] getClaims(UserPrincipal userPrincipal) {
+        List<String> authorities = new ArrayList<>();
+        for (GrantedAuthority grantedAuthority : userPrincipal.getAuthorities()) {
+            authorities.add(grantedAuthority.getAuthority());
+        }
+        return authorities.toArray(new String[0]);
+    }
+
+}
