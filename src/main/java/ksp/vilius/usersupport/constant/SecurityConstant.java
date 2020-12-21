7437c7675f92036943839b14b88bfee6e82f3a22
vilksp <v.kasperavicius1@gmail.com>
2020-12-21 16:41:00 +0200

    security constant added, jwt library in pom xml added

---
+++ src/main/java/ksp/vilius/usersupport/constant/SecurityConstant.java
@@ -0,0 +1,15 @@
+package ksp.vilius.usersupport.constant;
+
+public class SecurityConstant {
+    public static final Long EXPIRATION_TIME = 432_000_000L; //5 Days in millisecs
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String GET_ARRAYS_LLC = "Get Arrays,LLC";
+    public static final String GET_ADMINISTRATION = "User Management Portal";
+    public static final String AUTHORITIES = "authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
+    public static final String ACCESS_DENIED = "You don't have permission to access this page";
+    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+    public static final String[] PUBLIC_URLS = {"/api/user/login", "/api/user/register", "/api/user/resetpassword/**", "/api/user/image/**"};
+}
--- src/main/java/ksp/vilius/usersupport/controllers/UserController.java
+++ src/main/java/ksp/vilius/usersupport/controllers/UserController.java
@@ -8,4 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
+
 }
