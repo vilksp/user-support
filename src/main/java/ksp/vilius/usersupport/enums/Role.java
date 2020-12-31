176444c1e6f057b2bb273a1ad6d359f3f14fb8bd
vilksp <v.kasperavicius1@gmail.com>
2020-12-31 22:08:25 +0200

    Roles and authorities added

--- src/main/java/ksp/vilius/usersupport/enums/Role.java
+++ src/main/java/ksp/vilius/usersupport/enums/Role.java
@@ -1,2 +1,27 @@
-package ksp.vilius.usersupport.enums;public enum Role {
+package ksp.vilius.usersupport.enums;
+
+import static ksp.vilius.usersupport.constant.Authority.*;
+
+public enum Role {
+
+    ROLE_USER(USER_AUTHORITIES),
+
+    ROLE_HR(HR_AUTHORITIES),
+
+    ROLE_MANAGER(MANAGER_AUTHORITIES),
+
+    ROLE_ADMIN(ADMIN_AUTHORITIES),
+
+    ROLE_SUPER_ADMIN(SUPER_ADMIN_AUTHORITIES);
+
+    private String[] authorities;
+
+    Role(String... authority) {
+        this.authorities = authority;
+    }
+
+    public String[] getAuthorities() {
+        return authorities;
+    }
+
 }
