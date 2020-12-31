c7719c7e2d9bd283b579af243a1f5a4b74740ada
vilksp <v.kasperavicius1@gmail.com>
2020-12-31 22:07:54 +0200

    jwt added

--- src/main/java/ksp/vilius/usersupport/filter/JwtAuthenticationEntryPoint.java
+++ src/main/java/ksp/vilius/usersupport/filter/JwtAuthenticationEntryPoint.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ksp.vilius.usersupport.models.HttpResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,7 @@ import static ksp.vilius.usersupport.constant.SecurityConstant.FORBIDDEN_MESSAGE
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Component
 public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
 
     @Override
