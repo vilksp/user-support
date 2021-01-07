ba7e97a87708ab7b45cdc571f9320f154e0ef99a
vilksp <v.kasperavicius1@gmail.com>
2021-01-07 23:21:24 +0200

    service added with basic login/register

---
+++ frontend/src/app/service/authentication.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../model/User';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  private host = environment.apiUrl;
+  private token: string;
+  constructor(private http: HttpClient) {}
+
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>(
+      `{$this.host}/api/user/login`,
+      user,
+      { observe: 'response' }
+    );
+  }
+
+  public register(user: User): Observable<User | HttpErrorResponse> {
+    return this.http.post<User | HttpErrorResponse>(
+      `{$this.host}/api/user/register`,
+      user
+    );
+  }
+
+  public logout(): void {
+    this.token = null;
+    localStorage.clear();
+  }
+}
