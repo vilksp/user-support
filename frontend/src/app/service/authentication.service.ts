3c7ca6daa16191d36ab755fbfbfc1ecd1eb88832
vilksp <v.kasperavicius1@gmail.com>
2021-01-08 00:04:17 +0200

    methods to store token in localStorage and to check if jwt token is valid added

--- frontend/src/app/service/authentication.service.ts
+++ frontend/src/app/service/authentication.service.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +16,7 @@ export class AuthenticationService {
   private host = environment.apiUrl;
   private token: string;
   private loggedInUsername: string;
+  private jwtHelper = new JwtHelperService();
   constructor(private http: HttpClient) {}
 
   public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
@@ -38,5 +40,40 @@ export class AuthenticationService {
     localStorage.removeItem('user');
     localStorage.removeItem('token');
     localStorage.removeItem('users');
+  }
+
+  public saveToken(token: string): void {
+    this.token = token;
+    localStorage.setItem('token', token);
+  }
+
+  public addToLocalCache(user: User): void {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  public getUserFromLocalCache(): User {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+
+  public loadToken(): void {
+    this.token = localStorage.getItem('token');
+  }
+
+  public getToken(): string {
+    return localStorage.getItem('token');
+  }
+
+  public isLoggedIn(): boolean {
+    this.loadToken();
+    if (this.token != null && this.token !== '') {
+      if (this.jwtHelper.decodeToken(this.token).sub != null || '') {
+        if (this.jwtHelper.isTokenExpired(this.token)) {
+          this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub;
+          return true;
+        }
+      }
+    }
+    this.logout();
+    return false;
   }
 }
