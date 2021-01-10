e56ad275ca83c77ad039649c52d5524ac536d33a
vilksp <v.kasperavicius1@gmail.com>
2021-01-10 22:06:08 +0200

    guard added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { AuthenticationService } from './service/authentication.service';
 import { UserService } from './service/user.service';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
+import { AuthenticationGuard } from './guard/authentication.guard';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
+    AuthenticationGuard,
     AuthenticationService,
     UserService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
